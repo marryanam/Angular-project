cbbf73e0a50295f36d47c2bc3320137624561b9f
marryana <marryana.m@gmail.com>
2024-08-23 19:49:33 +0300

    design page 1

---
+++ src/app/pages/design-page/design-page-1/design-page-1.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-design-page-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './design-page-1.component.html',
+  styleUrl: './design-page-1.component.scss'
+})
+export class DesignPage1Component {
+
+}
--- src/app/pages/design-page/design-page.component.ts
+++ src/app/pages/design-page/design-page.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { PaginationComponent } from '../../components/pagination/pagination.component';
 import { PaginationService } from '../../services/pagination/pagination.service';
 
 @Component({
   selector: 'app-design-page',
   standalone: true,
-  imports: [PaginationComponent],
+  imports: [PaginationComponent, RouterOutlet],
   templateUrl: './design-page.component.html',
   styleUrl: './design-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
