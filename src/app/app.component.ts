ad551fccfdd8359e06155bd8a3fd6ba6574c5fc0
marryana <marryana.m@gmail.com>
2024-08-23 22:55:37 +0300

    design page 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { CustomCursorDirective } from './directives/cursor/custom-cursor.directive';
@@ -8,9 +8,47 @@ import { CustomCursorDirective } from './directives/cursor/custom-cursor.directi
   standalone: true,
   imports: [RouterOutlet, RouterLink, ButtonModule, CustomCursorDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = 'Angular-project';
+  private audio = new Audio();
+  private isAudioPlayed = false;
+  isAudioPlayedManual = false;
+
+  @HostListener('document:mousemove')
+  private startAudio(): void {
+    if (!this.isAudioPlayed) {
+      this.audio.play().then(() => {
+        this.isAudioPlayed = true;
+      }).catch(error => {
+        console.error('Failed to play audio:', error);
+      });
+    }
+  }
+
+  ngOnInit(): void {
+    this.audio.src = '../assets/Michael_Vignola_-_Knowing_51058718.mp3';
+    this.audio.load();
+    this.audio.loop = true; // Додаємо автоматичне повторення
+
+    // Перевірка на завершення аудіо і його перезапуск
+    this.audio.addEventListener('ended', () => {
+      this.audio.currentTime = 0;
+      this.audio.play().catch(error => {
+        console.error('Failed to replay audio:', error);
+      });
+    });
+  }
+
+  toggleAudio(){
+    this.isAudioPlayedManual = !this.isAudioPlayedManual;
+    this.isAudioPlayed = this.isAudioPlayedManual;
+    if(this.isAudioPlayedManual){
+      this.audio.pause();
+    } else{
+      this.startAudio();
+    }
+  }
 }
