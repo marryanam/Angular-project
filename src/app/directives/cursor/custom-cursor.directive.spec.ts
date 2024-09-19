856ff20c8e5bcd950b913cca167c871e77c6a8d8
marryana <marryana.m@gmail.com>
2024-09-19 18:04:58 +0300

    ci

--- src/app/directives/cursor/custom-cursor.directive.spec.ts
+++ src/app/directives/cursor/custom-cursor.directive.spec.ts
@@ -1,8 +1,19 @@
+import { TestBed } from '@angular/core/testing';
 import { CustomCursorDirective } from './custom-cursor.directive';
+import { ElementRef, Renderer2 } from '@angular/core';
 
 describe('CustomCursorDirective', () => {
+  let directive: CustomCursorDirective;
+  let el: ElementRef;
+  let renderer: Renderer2;
+
+  beforeEach(() => {
+    el = new ElementRef(document.createElement('div'));
+    renderer = jasmine.createSpyObj('Renderer2', ['setStyle']);
+    directive = new CustomCursorDirective(el, renderer);
+  });
+
   it('should create an instance', () => {
-    const directive = new CustomCursorDirective();
     expect(directive).toBeTruthy();
   });
 });
